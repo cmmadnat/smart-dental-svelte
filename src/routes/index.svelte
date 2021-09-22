08197131867f9050c3d447b360b5050445883c69
cmmadnat <cmmadnat@gmail.com>
2021-09-22 14:47:31 +0700

    add initial style

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,20 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<button
-	class="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-	type="button"
->
-	<i class="fas fa-heart" /> Small
-</button>
-<button
-	class="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-	type="button"
->
-	<i class="fas fa-heart" /> Regular
-</button>
-<button
-	class="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-base px-8 py-3 rounded shadow-md hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-	type="button"
->
-	<i class="fas fa-heart" /> Large
-</button>
+<div class="container mx-auto flex justify-center">
+	<div class="p-3 mt-40 w-1/2 object-center bg-white shadow-lg flex justify-center">
+		<img src="/Mahidol_dentis_th.png" class="shadowfilter" alt="" />
+	</div>
+</div>
+
+<style>
+	.shadowfilter {
+		-webkit-filter: drop-shadow(4px 2px 2px rgba(0, 0, 0, 0.5));
+		filter: drop-shadow(4px 2px 2px rgba(0, 0, 0, 0.5));
+	}
+</style>
