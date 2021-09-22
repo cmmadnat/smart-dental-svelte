0c9ce1489dad366398552045f4c591b7837eb84d
cmmadnat <cmmadnat@gmail.com>
2021-09-22 11:42:08 +0700

    add tailwind and notus thing

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,20 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button
+	class="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+	type="button"
+>
+	<i class="fas fa-heart" /> Small
+</button>
+<button
+	class="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+	type="button"
+>
+	<i class="fas fa-heart" /> Regular
+</button>
+<button
+	class="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-base px-8 py-3 rounded shadow-md hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+	type="button"
+>
+	<i class="fas fa-heart" /> Large
+</button>
