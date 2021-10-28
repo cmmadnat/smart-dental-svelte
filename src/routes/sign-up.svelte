4f54a68c7e05cd5bf9f3748c018a0d612090b5df
cmmadnat <cmmadnat@gmail.com>
2021-10-28 17:42:46 +0700

    add sign up page

--- src/routes/sign-up.svelte
+++ src/routes/sign-up.svelte
@@ -5,6 +5,8 @@
 	import SelectBox from './_SelectBox.svelte';
 	import LanguageSetting from './_SetLanguage.svelte';
 	import { onMount } from 'svelte';
+	import TextBox from './_TextBox.svelte';
+	import ButtonGreen from './_ButtonGreen.svelte';
 	let gender = 'm';
 	let titles;
 	onMount(() => {
@@ -13,19 +15,28 @@
 			.then((data) => Papa.parse(data.text, { header: true }))
 			.then((data) => data.data)
 			.then((data) => (titles = data.map((it) => ({ label: it.TITLE, id: it.CODE }))));
+
+		hcaptcha.render('captcha'); // eslint-disable-line no-undef
 	});
 </script>
 
+<svelte:head>
+	<script src="https://js.hcaptcha.com/1/api.js" async defer></script>
+	<meta
+		http-equiv="Content-Security-Policy"
+		content="script-src 'self' 'unsafe-eval' 'unsafe-inline' https://hcaptcha.com https://*.hcaptcha.com; frame-src 'self' https://hcaptcha.com https://*.hcaptcha.com; style-src 'self' 'unsafe-inline' https://hcaptcha.com https://*.hcaptcha.com"
+	/>
+</svelte:head>
 <div class="container mx-auto flex justify-center ">
 	<div
-		class="p-3 mt-40 w-full lg:w-1/2 object-center bg-white shadow-lg flex justify-center flex-col items-center"
+		class="p-3 my-10 w-full lg:w-1/2 object-center bg-white shadow-lg flex justify-center flex-col items-center"
 	>
 		<img src="/Mahidol_dentis_th.png" class="w-96 shadowfilter" alt="" />
 		<hr class="w-full my-2" />
 		<h1 class="text-2xl bold text-center my-5">
 			{$_('signup')}
 		</h1>
-		<div class="w-full flex flex-row">
+		<div class="w-full flex flex-row flex-wrap">
 			<div class="w-1/2">
 				<div class="text-center py-5">
 					<p class="my-2">
@@ -38,8 +49,55 @@
 					</p>
 				</div>
 			</div>
-			<div class=" w-1/2">
+			<div class=" w-1/2 p-2">
 				<SelectBox label={$_('title')} options={titles} />
+			</div>
+			<div class=" w-1/2 p-2">
+				<TextBox label={$_('firstName')} />
+			</div>
+			<div class=" w-1/2 p-2">
+				<TextBox label={$_('lastName')} />
+			</div>
+			<div class=" w-1/2 p-2">
+				<SelectBox
+					label={$_('IDCard')}
+					options={[
+						{ label: 'บัตรประชาชน', id: '1' },
+						{ label: 'หนังสือเดินทาง', id: '2' }
+					]}
+				/>
+			</div>
+			<div class="p-2 w-1/2">
+				<TextBox label={$_('cardNumber')} />
+			</div>
+			<div class="p-2 w-1/2">
+				<TextBox label={$_('phone')} />
+			</div>
+			<div class="p-2 w-1/2">
+				<TextBox label={$_('email')} />
+			</div>
+			<div class="p-2 w-1/2">
+				<TextBox label={$_('password')} />
+			</div>
+			<div class="p-2 w-1/2">
+				<TextBox label={$_('verifyPassword')} />
+			</div>
+			<hr class="w-full my-2" />
+			<div class="text-center w-full flex justify-center">
+				<div id="captcha" class="h-captcha" data-sitekey="0ac5cabf-2e30-4ca0-90ee-11ba693777e6" />
+			</div>
+
+			<hr class="w-full my-2" />
+			<div class="items-center flex-col flex justify-center  w-full">
+				<div>
+					<input type="checkbox" id="agreement" />
+
+					<label for="agreement">{$_('agreeTo')}</label>
+					<a class="underline" href="/" target="_blank">{$_('userAgreement')}</a>
+				</div>
+				<div class="w-1/2 text-center my-2">
+					<ButtonGreen>{$_('agreeAndConfirm')}</ButtonGreen>
+				</div>
 			</div>
 		</div>
 
