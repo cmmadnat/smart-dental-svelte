c4c55a1dd9def9799f5d33bf5e14cdd2a0981a44
cmmadnat <cmmadnat@gmail.com>
2021-12-07 15:09:14 +0700

    add the same address type

--- src/routes/NewPatient/_AddressInfo.svelte
+++ src/routes/NewPatient/_AddressInfo.svelte
@@ -20,6 +20,13 @@
 	export let workPhone = '';
 	export let emergencyContact = '';
 	export let emergencyRelationship = '';
+	let sameAddress = false;
+	const copyAddress = () => {
+		emergencyAddressType = addressType;
+		emergencyAddressLineOne = addressLineOne;
+		emergencyPostalCode = postalCode;
+		emergencySubdistrict = subdistrict;
+	};
 
 	const addressTypes = ['ที่อยู่ตามทะเบียนบ้าน', 'อื่นๆ'].map((it, index) => ({
 		id: index + 1 + '',
@@ -30,7 +37,7 @@
 <div class="bg-blue-200">
 	<h1 class="bold text-2xl w-full p-2">{$_('contactInfo')}</h1>
 </div>
-<div class="flex flex-row flex-wrap">
+<div class="flex flex-row flex-wrap pb-2">
 	<div class="w-1/3 px-2">
 		<SelectBox bind:value={addressType} label={$_('addressType')} options={addressTypes} />
 	</div>
@@ -57,20 +64,32 @@
 		<TextBox bind:value={emergencyRelationship} label={$_('relationship')} />
 	</div>
 	<div class="w-full">
-		<input type="checkbox" id="sameAddressAsMe" />
+		<input
+			bind:checked={sameAddress}
+			on:change={copyAddress}
+			type="checkbox"
+			id="sameAddressAsMe"
+		/>
 		<label for="sameAddressAsMe">{$_('sameAddressAsMe')}</label>
 	</div>
-	<div class="w-1/3 px-2">
-		<SelectBox bind:value={emergencyAddressType} label={$_('addressType')} options={addressTypes} />
-	</div>
-	<div class="w-2/3 px-2">
-		<TextBox bind:value={emergencyAddressLineOne} label={$_('addressLineOne')} />
-	</div>
-	<div class="px-2 w-full">
-		<CityPicker
-			bind:postalCode={emergencyPostalCode}
-			bind:selectedSubdistrict={emergencySubdistrict}
-			bind:value={addressLineTwo}
-		/>
-	</div>
+
+	{#if !sameAddress}
+		<div class="w-1/3 px-2">
+			<SelectBox
+				bind:value={emergencyAddressType}
+				label={$_('addressType')}
+				options={addressTypes}
+			/>
+		</div>
+		<div class="w-2/3 px-2">
+			<TextBox bind:value={emergencyAddressLineOne} label={$_('addressLineOne')} />
+		</div>
+		<div class="px-2 w-full">
+			<CityPicker
+				bind:postalCode={emergencyPostalCode}
+				bind:selectedSubdistrict={emergencySubdistrict}
+				bind:value={addressLineTwo}
+			/>
+		</div>
+	{/if}
 </div>
