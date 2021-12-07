16aa12e1c67b980412e17a80d28d2f719e308e33
cmmadnat <cmmadnat@gmail.com>
2021-12-07 20:08:25 +0700

    add quill editor

---
+++ src/lib/_SaveResetBack.svelte
@@ -0,0 +1,31 @@
+<script>
+	import ButtonGreen from './_ButtonGreen.svelte';
+	import ButtonBlue from './_ButtonBlue.svelte';
+	import ButtonRed from './_ButtonRed.svelte';
+	import { createEventDispatcher } from 'svelte';
+	import { _ } from 'svelte-i18n';
+	const dispatcher = createEventDispatcher();
+	const save = () => {
+		dispatcher('save');
+	};
+	const reset = () => {
+		dispatcher('reset');
+	};
+</script>
+
+<div class="flex flex-row justify-between">
+	<div>
+		<ButtonGreen on:click={save}><i class="lni lni-save" /> {$_('save')}</ButtonGreen>
+		<ButtonRed on:click={reset}><i class="lni lni-archive" /> {$_('reset')}</ButtonRed>
+	</div>
+	<div class="justify-self-end ">
+		<ButtonBlue
+			on:click={() => {
+				history.back();
+			}}
+		>
+			<i class="lni lni-backward" />
+			{$_('return')}</ButtonBlue
+		>
+	</div>
+</div>
--- src/routes/shopping/items/[id].svelte
+++ src/routes/shopping/items/[id].svelte
@@ -3,12 +3,43 @@
 	import Header from '$lib/_Navigation.svelte';
 	import LanguageSetting from '$lib/_SetLanguage.svelte';
 	import Menu from '$lib/_menu.svelte';
+	import { onMount } from 'svelte';
+	import TextBox from '$lib/_TextBox.svelte';
+	import SaveResetBack from '$lib/_SaveResetBack.svelte';
+
+	let editor;
+
+	export let toolbarOptions = [
+		[{ header: 1 }, { header: 2 }, 'blockquote', 'link', 'image', 'video'],
+		['bold', 'italic', 'underline', 'strike'],
+		[{ list: 'ordered' }, { list: 'ordered' }],
+		[{ align: [] }],
+		['clean']
+	];
+
+	let quill;
+	onMount(async () => {
+		const { default: Quill } = await import('quill');
+
+		quill = new Quill(editor, {
+			modules: {
+				toolbar: toolbarOptions
+			},
+			theme: 'snow',
+			placeholder: 'Write your story...'
+		});
+	});
+	const save = () => {
+		console.log(quill.root.innerHTML);
+	};
 </script>
 
 <Header />
 <div class="lg:container mx-auto bg-white p-2 rounded-lg shadow-lg">
 	<div class="flex justify-between">
-		<h1 class="text-2xl bold">{$_('appName')}</h1>
+		<h1 class="text-2xl bold">
+			{$_('addEditItem')}
+		</h1>
 		<LanguageSetting />
 	</div>
 	<hr />
@@ -17,6 +48,19 @@
 		<div class="md:w-1/3">
 			<Menu />
 		</div>
-		<div class="md:w-1/3 w-full" />
+		<div class="md:w-2/3 w-full">
+			<SaveResetBack on:save={save} />
+			<TextBox label={$_('itemName')} />
+			<div class="my-2">
+				<span class="">{$_('itemDescription')}</span>
+			</div>
+			<div class="editor-wrapper">
+				<div bind:this={editor} />
+			</div>
+		</div>
 	</div>
 </div>
+
+<style>
+	@import 'https://cdn.quilljs.com/1.3.6/quill.snow.css';
+</style>
