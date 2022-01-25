109db0841267a359c3e09ac2c74dd4d5d4bb98ee
cmmadnat <cmmadnat@gmail.com>
2022-01-25 09:33:20 +0700

    remove remaining setup file

--- src/hooks.ts
+++ src/hooks.ts
@@ -2,10 +2,7 @@ import 'reflect-metadata';
 import { connectionToDatabase } from '$lib/database';
 
 import type { Handle } from '@sveltejs/kit/types/hooks';
-import setupTitles from '$lib/setupTitles';
 import { getConnection } from 'typeorm';
-import setupReligions from '$lib/setupReligions';
-import setupOccupations from '$lib/setupOccupations';
 import { setupDrug } from '$lib/setupDrug';
 
 export const handle: Handle = ({ request, resolve }) => {
@@ -15,9 +12,6 @@ export const handle: Handle = ({ request, resolve }) => {
 		console.log('Connecting to database...');
 		connectionToDatabase()
 			.then(async () => {
-				await setupTitles();
-				await setupReligions();
-				await setupOccupations();
 				await setupDrug();
 			})
 			.then(() => console.log('database connected'));
